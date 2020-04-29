f02cd5b0c5e192272db6161ec521a4f1e94d5ace
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-29 15:01:15 +0200

    Update rechercher.php

--- ProjetBDD/rechercher.php
+++ ProjetBDD/rechercher.php
@@ -21,7 +21,7 @@
     public function getTitre($titre)
     {
         $titre = (string) $titre;
-        $q = $this->_db->query('SELECT * from Film where titre='.$titre);
+        $q = $this->_db->query('SELECT * from Film where UPPER(titre) LIKE'.$titre.'%');
         $donnees = $q->fetch(PDO::FETCH_ASSOC);
 
         $a = new Film($donnees);
@@ -30,16 +30,23 @@
     }
     public function Rechercher($nom)
     {
+        $nom = (string)$nom;
+        $q = $this->_db->query('SELECT filmotheque.idFilm, filmotheque.titre,dateSortie, filmotheque.nationaliteFilm, filmotheque.duree, filmotheque.descriptionFilm, filmotheque.noteFilm FROM filmotheque.film  WHERE UPPER(filmotheque.titre) LIKE'.$nom.'%;');
 
-        $q = $this->_db->query('SELECT idFilm, titre,dateSortie, nationaliteFilm, duree,descriptionFilm,noteFilm FROM filmotheque.film 
-                                WHERE filmotheque.titre = '.$nom);
-
-        $donnees = $q->fetch(PDO::FETCH_ASSOC);
+        /*$donnees = $q->fetch(PDO::FETCH_ASSOC);
 
         $a = new Film($donnees);
 
         echo $q->bindValue(':titre', $a->getTitre());
-        return $a;
+        return $a;*/
+
+        $titre = (string) $nom;
+        $q = $this->_db->query('SELECT * from Film where UPPER(titre) LIKE'.$titre.'%');
+        $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+        $a = new Film($donnees);
+
+        return $this->d;
     }
 
  }
